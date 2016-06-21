0ac6674a07b442596a3931cdaaa8adba5b4cb61a
Ryan Magee <ryanmag1@yahoo.com>
2016-06-21 16:59:30 -0600

    Made dashboard homepage and added logo to navigation.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :daily_totals
   get 'dashboards/get_usage', as:'dashboard'
   post 'update_usage' => 'dashboards#get_usage'
+  root 'dashboards#get_usage'
+
 
 end
